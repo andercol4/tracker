08436096f8c16ed8434d203c64208a2132e974c0
Colin Anderson <andercol.4@gmail.com>
2016-03-10 10:13:40 -0700

    got a custom admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  
-  namespace :admin do
+  namespace :admin, path: 'tracker' do
     resources :users
     resources :seizures
 
     root to: "users#index"
   end
+
+  # namespace :admin, path: 'zombie-llama' do
+  #  DashboardManifest.new.dashboards.each do |dashboard_resource|
+  #    resources dashboard_resource
+  #  end
+  #    root controller: DashboardManifest.new.root_dashboard, action: :index
+  #  end
 
   root 'home#index'
   devise_for :users
