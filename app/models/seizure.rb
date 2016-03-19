35207c2da009727115dd7d1f2fe22cb4790e3f8f
Colin Anderson <andercol.4@gmail.com>
2016-03-18 18:14:37 -0600

    fixed a model saving error

--- app/models/seizure.rb
+++ app/models/seizure.rb
@@ -1,35 +1,60 @@
 class Seizure < ActiveRecord::Base
   acts_as_taggable
-  validate :seizure_date_cannot_be_in_the_future
-  validates :date, :length, presence: true
-  def seizure_date_cannot_be_in_the_future
-    if self.date < DateTime.now
-      errors.add(:expiration_date, "can't be in the past")
-    end
-  end
+  validates_presence_of :date
+  validates_presence_of :length
+  # validate :seizure_date_cannot_be_in_the_future
+  # def seizure_date_cannot_be_in_the_future
+  #   if self.date > DateTime.now
+  #     errors.add(:expiration_date, "can't be in the past")
+  #   end
+  # end
 
 
   def simple_time
-    self.date.strftime("%l:%M %P")
+    if self.date
+      self.date.strftime("%l:%M %P")
+    else
+      'N/A'
+    end
   end
   def simple_date
-    self.date.strftime("%b %-d, %Y")
+    if self.date
+      self.date.strftime("%b %-d, %Y")
+    else
+      'N/A'
+    end
   end
   def parsed_length
-    minutes = self.length.to_s.split('')
-    seconds = minutes.pop(2)
-    minutes = minutes.join('')
-    second_percent = seconds.join('').to_i / 100.0 * 60
-    second_percent = second_percent.to_i.to_s
-    time = minutes + ':' + second_percent
+    if self.length != 0
+      minutes = self.length.to_s.split('')
+      seconds = minutes.pop(2)
+      minutes = minutes.join('')
+      second_percent = seconds.join('').to_i / 100.0 * 60
+      second_percent = second_percent.to_i.to_s
+      if second_percent = '0'
+        time = "#{minutes}:#{second_percent}0"
+      else
+        time = "#{minutes}:#{second_percent}"
+      end
+    else
+      '0:00'
+    end
   end
   def encrypt_length
-    minutes = self.length.to_s.split('')
-    seconds = minutes.pop(2)
-    second_percent = seconds.join('').to_i / 60.0 * 100
-    second_percent = second_percent.to_i.to_s
-    minutes = minutes.join('')
-    time = minutes + second_percent
+    if self.length
+      minutes = self.length.to_s.split('')
+      seconds = minutes.pop(2)
+      second_percent = seconds.join('').to_i / 60.0 * 100
+      second_percent = second_percent.to_i.to_s
+      minutes = minutes.join('')
+      if second_percent = '0'
+        time = minutes + second_percent + '0'
+      else
+        time = minutes + second_percent
+      end
+    else
+      '000'
+    end
   end
 
   def self.get_by_time(time = 30)
